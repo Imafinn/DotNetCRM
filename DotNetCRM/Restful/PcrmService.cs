005480e514c81147a4f61d7ea494fb68a4abf7b8
Holyoneqt <spuehler.luca@gmail.com>
2017-04-27 08:44:13 +0200

    Implemented Restful

---
+++ DotNetCRM/Restful/PcrmService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using DataModels.Entities;
+using Database.DataAccess;
+
+namespace Restful
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class PcrmService : IPcrmService
+    {
+        PcrmDao _pcrmDao = new PcrmDao();
+
+        public RestEmployee GetEmployee(string id)
+        {
+            return new RestEmployee(_pcrmDao.GetEmployee(int.Parse(id)));
+        }
+    }
+}
